711c10733accea434dea27e1e1a91e2d0f57c504
Stan Fortonski <contact@stanfortonski.dev>
2021-03-30 21:10:03 +0200

    Start page improvement

--- src/components/pages/Start.vue
+++ src/components/pages/Start.vue
@@ -1,20 +1,28 @@
 <template>
-<div>
-  <v-card elevation="2" class="py-15 px-4 mb-4">
-    <h1 class="mb-2">Language Dictionary</h1>
-    <p>Made by Stan Fortoński</p>
-    <a href="https://github.com/stanfortonski/lang-dictionary" target="_blank">
-      <v-btn color="primary" elevation="1" x-large>
-        Learn more
-      </v-btn>
-    </a>
-  </v-card>
+  <div>
+    <v-card elevation="2" class="py-15 px-4 mb-4">
+      <h1 class="mb-4">Language Dictionary</h1>
+      <div>
+        <router-link to="/dictionary">
+          <v-btn color="primary" elevation="1" class="mx-2 my-2">dictionary</v-btn>
+        </router-link>
+        <router-link to="/random-word">
+          <v-btn color="primary" elevation="1" class="mx-2 my-2">Random word</v-btn>
+        </router-link>
+          <router-link to="/guess-word">
+          <v-btn color="primary" elevation="1" class="mx-2 my-2">Guess word</v-btn>
+        </router-link>
+      </div>
+    </v-card>
 
-  <v-card elevation="2" class="py-8 px-4">
-    <router-link to="/dictionary">
-      <v-btn color="primary" elevation="1" x-large>go to dictionary</v-btn>
-    </router-link>
-  </v-card>
+    <v-card elevation="2" class="py-8 px-4">
+      <p>Made by Stan Fortoński</p>
+      <a href="https://github.com/stanfortonski/lang-dictionary" target="_blank">
+        <v-btn color="primary" elevation="1">
+          Read about it
+        </v-btn>
+      </a>
+    </v-card>
   </div>
 </template>
 
